241cb3d719aba28516e0b056845c9294e25ce4d6
Davi Chaves <dchaves@usc.edu>
2020-07-17 11:51:19 -0700

    Initial Commit

---
+++ WeatherDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherDemo
+//
+//  Created by Davi Chaves on 7/17/20.
+//  Copyright © 2020 Davi Chaves. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
