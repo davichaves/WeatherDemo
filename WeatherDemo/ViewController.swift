7840317f91057d754db66576847817e919d18213
Davi Chaves <dchaves@usc.edu>
2020-07-17 12:18:10 -0700

    map with current location

--- WeatherDemo/ViewController.swift
+++ WeatherDemo/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkLocationServices()
+    }
+
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkLocationAuthorization()
+        } else {
+            // Show alert letting the user know they have to turn this on.
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            map.showsUserLocation = true
+        case .denied:
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            map.showsUserLocation = true
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        }
     }
 
 
